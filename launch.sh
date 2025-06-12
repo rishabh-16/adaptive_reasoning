a48b63b358105dd84bb69b95dbf810370f6d2e56
rishabh-16 <rishabhtiwari@meta.com>
2025-06-12 15:39:17 +0000

    added experiment structure

--- launch.sh
+++ launch.sh
@@ -2,8 +2,7 @@
 
 EXPERIMENT=$1
 N_TASKS=$2
-JOB_NAME=$3
-GPUS_PER_TASK=$4
+GPUS_PER_TASK=$3
 
 if [ -z "$N_TASKS" ]; then
     N_TASKS=1
@@ -13,16 +12,12 @@ if [ -z "$GPUS_PER_TASK" ]; then
     GPUS_PER_TASK=1
 fi
 
-if [ -z "$JOB_NAME" ]; then
-    JOB_NAME=$EXPERIMENT
-fi
-
 if [ -z "$EXPERIMENT" ]; then
     echo "Usage: $0 <experiment>"
     exit 1
 fi
 
-chmod +x experiments/$EXPERIMENT.sh
-sbatch --account=genai_interns --qos=genai_interns --job-name=$JOB_NAME --ntasks=$N_TASKS --gpus-per-task=$GPUS_PER_TASK experiments/$EXPERIMENT.sh $GPUS_PER_TASK
+chmod +x experiments/$EXPERIMENT/run.sh
+sbatch --account=genai_interns --qos=genai_interns --job-name=$EXPERIMENT --ntasks=$N_TASKS --gpus-per-task=$GPUS_PER_TASK --output=experiments/$EXPERIMENT/%x_%j.out experiments/$EXPERIMENT/run.sh $GPUS_PER_TASK
 
-echo "Job submitted with job name: $JOB_NAME, ntasks: $N_TASKS, gpus per task: $GPUS_PER_TASK"
+echo "Job submitted with job name: $EXPERIMENT, ntasks: $N_TASKS, gpus per task: $GPUS_PER_TASK"
